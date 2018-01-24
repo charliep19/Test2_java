d72bcc22bdaa47e8ce06bf687baba2d4fa62573c
Student <Student>
2018-01-24 15:30:00 -0500

    first

---
+++ src/exercise2.java
@@ -0,0 +1,10 @@
+
+public class exercise2 {
+
+	public static void main(String[]args){
+		
+		for( int i = 1; i<= 33; i++){
+			System.out.println(i);
+		}
+	}
+}
---
+++ src/work1.java
@@ -0,0 +1,18 @@
+
+public class work1 {
+	public static void main(String[]args){
+		int n = 1;
+		int squareValue;
+		int sum = 0;
+		while (n <= 10) {
+
+		    squareValue= (n*n);
+
+		    sum += squareValue; 
+
+		    n++;
+		}
+		System.out.println(sum);
+	}
+
+}
